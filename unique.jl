52b62d0bc55014e348451b6b1446bd17e49a3c72
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-09 18:04:42 +0900

    Create unique.jl

---
+++ unique.jl
@@ -0,0 +1,17 @@
+
+input_line = readline()
+S = Set(input_line)
+
+found_unique = false
+
+for s in S
+    if count(==(s), input_line) == 1
+        println(s)
+        found_unique = true
+        break
+    end
+end
+
+if !found_unique
+    println("-1")
+end
